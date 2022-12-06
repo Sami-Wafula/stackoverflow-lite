1067a6cfe2bfe5f1860db94b9aac9c8da250ee7c
Sami Wafula <samiwafula@Mwovi.local>
2022-12-06 08:53:59 +0300

    add about page

---
+++ src/components/about/About.js
@@ -0,0 +1,22 @@
+import React from "react";
+import './About.css'
+
+const About = () =>{
+    return(
+        <>
+        <div className="about">
+            <div>
+                <h5>StackOverflow-lite is a developer community where developers<br/> 
+                ask and answer questions related to various programming topics.<br/>
+                We strongly believe that developer community engagement is key <br/>
+                to growth and development of skills via exchange of ideas.<br/>
+                 Our vast databases act as reliable, peer-reviewed and <br/>
+                 up-to-date documentation to technnological resources with <br/>
+                 scarce official documentation.</h5>
+            </div>
+        </div>
+        </>
+    )
+}
+
+export default About;
