61659f8933f838a62ea17f7de68c2e69b413713c
Sami Wafula <samiwafula@Mwovi.local>
2022-12-06 09:20:04 +0300

    update

--- src/App.js
+++ src/App.js
@@ -1,11 +1,32 @@
 // import logo from './logo.svg';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './App.css';
 import NavBar from './components/navbar/NavBar';
+import About from './components/about/About';
+import LandingPage from './components/landingpage/LandingPage';
+import SignIn from './components/login/SignIn';
+import SignUp from './components/signup/SignUp';
+import SideNav from './components/sidenav/SideNav';
+import Questions from './components/questions/Questions';
+import Tags from './components/tags/Tags';
+import Users from './components/users/Users';
 
 function App() {
   return (
     <>
-    <NavBar/>
+    <NavBar />
+    <Routes>
+      <Route path='/home'  element={<LandingPage />} />
+      <Route path='/sidenav' element={<SideNav />} >
+
+      <Route path='/sidenav/users' element={<Users/>} />
+      <Route path='/sidenav/tags' element={<Tags />}/>
+      <Route path='/sidenav/questions' element={<Questions/>}/>
+      </Route>
+      <Route path='/about' element={<About/>}/>
+      <Route path='/signup' element={<SignUp/>} />
+      <Route path='/login' element={<SignIn/>} />
+    </Routes>
     </>
   );
 }
--- src/index.js
+++ src/index.js
@@ -2,13 +2,29 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import NavBar from './components/navbar/NavBar';
+import About from './components/about/About';
+import LandingPage from './components/landingpage/LandingPage';
+import SignIn from './components/login/SignIn';
+import SignUp from './components/signup/SignUp'
+import SideNav from './components/sidenav/SideNav';
+import Questions from './components/questions/Questions';
+import Tags from './components/tags/Tags';
+import Users from './components/users/Users';
 // import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+  // <React.StrictMode>
+    <BrowserRouter>
     <App />
-  </React.StrictMode>
+    {/* <Routes>
+        <Route path='/' element={<LandingPage />} />
+        <Route path='sidenav' element={<SideNav />} />
+      </Routes> */}
+    </BrowserRouter>  
+  // </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
